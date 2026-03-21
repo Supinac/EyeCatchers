c697c612fbfdd6c7c9afabb696bdabf1c1b649c6
Tobias Krč <supinacgaming@gmail.com>
2026-03-21 06:20:37 +0100

    fixed import mistake

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,24 +5,33 @@ from fastapi import FastAPI, Request, Response
 from .config import settings
 from . import db
 from . import endpoints
+from . import users
 
 
-if not db.ping(host=settings.DB_HOST,port=int(settings.DB_PORT)):
+if not db.ping(host=settings.DB_HOST, port=int(settings.DB_PORT)):
     print("ERROR: Database is not reachable.")
     sys.exit(1)
-    
-db.connect_mysql(user=settings.DB_USER, password=settings.DB_PASSWORD, host=settings.DB_HOST, port=settings.DB_PORT, db_name=settings.DB_NAME, pool_size=settings.DB_POOL_SIZE, max_overflow=settings.DB_POOL_OVERFLOW)
+
+db.connect_mysql(
+    user=settings.DB_USER,
+    password=settings.DB_PASSWORD,
+    host=settings.DB_HOST,
+    port=settings.DB_PORT,
+    db_name=settings.DB_NAME,
+    pool_size=settings.DB_POOL_SIZE,
+    max_overflow=settings.DB_POOL_OVERFLOW,
+)
 db.init()
 
 
-
 app = FastAPI(
-    title="Bleeper API", 
-    description="API for a simple microblogging app built with FastAPI.",
+    title="EyeCatchers API",
+    description="API for the EyeCatchers app.",
     version="1.0.0",
 )
 
 app.include_router(endpoints.router)
+app.include_router(users.router)
 
 @app.middleware("http")
 async def add_served_by_header(request: Request, call_next):
@@ -30,7 +39,3 @@ async def add_served_by_header(request: Request, call_next):
     hostname = socket.gethostname()
     response.headers["X-Served-By"] = hostname
     return response
-
-
-
-
