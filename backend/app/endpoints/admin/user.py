068b5a7f112caf3dc4f951f18f6775ed213f3092
Antonín Kříž <akriz@students.zcu.cz>
2026-03-21 12:56:30 +0100

    post score waiting for auth

--- backend/app/endpoints/admin/user.py
+++ backend/app/endpoints/admin/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app import tables
 from ... import db
-from ...models import  UserCreate, UserResponse, UserUpdate
+from ...models import  UserCreate, UserResponse, UserUpdate, ScoreSubmit, GameType
 
 
 router = APIRouter(prefix="/user", tags=["Admin - user"])
@@ -25,3 +25,14 @@ def update_user(id: int, user: UserUpdate, session: Session = Depends(db.session
 @router.delete("/{id}", status_code=204, response_model=None)
 def delete_user(id: int, session: Session = Depends(db.session)):
     pass
+
+@router.post("/{id}/score", status_code=201, response_model=None)
+def submit_score(id: int, score: ScoreSubmit, session: Session = Depends(db.session)):
+    session.add(tables.UserScore(
+        user_id=id,
+        game_type=score.game_type,
+        success_rate=score.success_rate,
+        difficulty=score.difficulty,
+        settings=score.settings,
+    ))
+    session.commit()
