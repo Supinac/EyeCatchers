0a2360527632083f8205f9c37500b3aabda98e2f
KarelRuzicka <karelruzicka2001@gmail.com>
2026-03-21 10:18:47 +0100

    Dependency fix

--- backend/app/endpoints/admin/user.py
+++ backend/app/endpoints/admin/user.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 from ... import db
-from ...models import User, UserCreate, UserLogin, UserResponse, UserUpdate
-from ...auth import hash_password, verify_password, create_token, get_current_user
+from ...models import  UserCreate, UserResponse, UserUpdate
 
 
 router = APIRouter(prefix="/user", tags=["Admin - user"])
--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ import socket
 from fastapi import FastAPI, Request, Response
 from .config import settings
 from . import db
-from endpoints import admin
+from .endpoints.admin import router as admin_router
 
 
 if not db.ping(host=settings.DB_HOST, port=int(settings.DB_PORT)):
@@ -38,5 +38,5 @@ app.add_middleware(
 )
 
 
-app.include_router(admin.user.router)
-app.include_router(admin.admin.router)
+# app.include_router(user.router)
+app.include_router(admin_router.router)
