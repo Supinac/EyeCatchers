1f763403915af47191f26c711699af5566e6fb70
Tobias Krč <supinacgaming@gmail.com>
2026-03-21 11:38:57 +0100

    publish

--- backend/app/tables.py
+++ backend/app/tables.py
@@ -10,13 +10,13 @@ class User(Base):
     __tablename__ = "user"
     id:         Mapped[int]       = mapped_column(primary_key=True, init=False)
     name:       Mapped[str]       = mapped_column(String(50))
-    user_name:  Mapped[str]       = mapped_column(String(50), unique=True)
+    login:  Mapped[str]       = mapped_column(String(50), unique=True)
     created_at: Mapped[datetime]  = mapped_column(server_default=func.now(), init=False)
 
 class Admin(Base):
     __tablename__ = "admin"
     id:         Mapped[int]       = mapped_column(primary_key=True, init=False)
-    user_name:  Mapped[str]       = mapped_column(String(50), unique=True)
+    login:  Mapped[str]       = mapped_column(String(50), unique=True)
     password:   Mapped[str]       = mapped_column(String(255))  # stored as bcrypt hash
     created_at: Mapped[datetime]  = mapped_column(server_default=func.now(), init=False)
 
