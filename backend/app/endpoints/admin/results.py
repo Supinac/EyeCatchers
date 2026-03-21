7a33c9bdec1ffda26b42dc7caf0ea9b26bc4f39b
Antonín Kříž <akriz@students.zcu.cz>
2026-03-21 14:12:07 +0100

    default admin + results get

--- backend/app/endpoints/admin/results.py
+++ backend/app/endpoints/admin/results.py
@@ -11,3 +11,13 @@ from ...admin_auth import hash_password, verify_password, create_token, auth_adm
 
 
 router = APIRouter(prefix="/results", tags=["Admin - Results"])
+
+@router.get("", status_code=200, response_model=list[tables.UserScore])
+def get_results(session: Session = Depends(db.session), current_admin: tables.Admin = Depends(auth_admin)):
+    results = session.execute(select(tables.UserScore)).scalars().all()
+    return results
+
+@router.get("/user/{user_id}", status_code=200, response_model=list[tables.UserScore])
+def get_user_results(user_id: int, session: Session = Depends(db.session), current_admin: tables.Admin = Depends(auth_admin)):
+    results = session.execute(select(tables.UserScore).where(tables.UserScore.user_id == user_id)).scalars().all()
+    return results
