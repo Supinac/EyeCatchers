29b2e550164b74f6b24037ecee51b97e1a4822c0
KarelRuzicka <karelruzicka2001@gmail.com>
2026-03-21 18:42:32 +0100

    Backend logout

--- backend/app/endpoints/admin/login.py
+++ backend/app/endpoints/admin/login.py
@@ -7,14 +7,14 @@ from sqlalchemy.orm import Session
 from app import tables
 from ... import db
 from ...models import AdminLogin, AdminUpdate, AdminResponse
-from ...admin_auth import auth_admin, verify_password, create_token, login_admin
+from ...admin_auth import auth_admin, verify_password, create_token, login_admin, logout_admin
 from ...config import settings
 
 
-router = APIRouter(prefix="/login", tags=["Admin - login"])
+router = APIRouter(prefix="", tags=["Admin - login"])
 
 
-@router.post("", status_code=200, response_model=AdminResponse)
+@router.post("/login", status_code=200, response_model=AdminResponse)
 def admin_login(user: AdminLogin, response: Response, session: Session = Depends(db.session)):
 
     admin = login_admin(
@@ -24,3 +24,8 @@ def admin_login(user: AdminLogin, response: Response, session: Session = Depends
         session=session,
     )
     return admin
+
+@router.post("/logout", status_code=204, response_model=None)
+def admin_logout(response: Response):
+    logout_admin(response=response)
+    return None
--- backend/app/endpoints/user/login.py
+++ backend/app/endpoints/user/login.py
@@ -5,16 +5,16 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app import tables
-from app.user_auth import login_user
+from app.user_auth import login_user, logout_user
 from ... import db
 from ...models import UserLogin, UserResponse
 from ...config import settings
 
 
-router = APIRouter(prefix="/login", tags=["User - login"])
+router = APIRouter(prefix="", tags=["User - login"])
 
 
-@router.post("", status_code=200, response_model=UserResponse)
+@router.post("/login", status_code=200, response_model=UserResponse)
 def user_login(user: UserLogin, response: Response, session: Session = Depends(db.session)):
 
     user = login_user(
@@ -23,3 +23,8 @@ def user_login(user: UserLogin, response: Response, session: Session = Depends(d
         session=session,
     )
     return user
+
+@router.post("/logout", status_code=204, response_model=None)
+def user_logout(response: Response):
+    logout_user(response=response)
+    return None
